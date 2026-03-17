8e979d414afb78e49fa10b82f48f947931f53abe
Karan Sanghvi <karansanghvi1303@gmail.com>
2026-03-16 21:55:09 -0700

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import './App.css'
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
-function App() {
-
+function SpinningCube() {
+  const ref = useRef();
+  useFrame(() => {
+    ref.current.rotation.x += 0.01;
+  })
   return (
-    <>
-     
-    </>
-  )
+    <mesh ref={ref}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
 }
 
-export default App
+export default function App() {
+  return (
+    <>
+    <Canvas style={{ width: '100vw', height: '100vh' }}>
+      <ambientLight intensity={1} />
+      <SpinningCube />
+    </Canvas>
+    </>
+  );
+}
